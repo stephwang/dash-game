47d0ad4d5542eb67e00574fed5cf12147d5c0639
Stephanie Wang <stephwang@dropbox.com>
2017-05-09 20:48:48 -0700

    respawn

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -5,9 +5,19 @@ using System.Collections;
 
 public class Health : NetworkBehaviour {
 	public const int maxHealth = 90;
+
 	[SyncVar(hook = "OnChangeHealth")]
 	public int currentHealth = maxHealth;
+
 	public RectTransform healthBar;
+
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start () {
+		if (isLocalPlayer) {
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+		}
+	}
 
 	public void TakeDamage(int amount) {
 		if (!isServer) {
@@ -16,12 +26,34 @@ public class Health : NetworkBehaviour {
 
 		currentHealth -= amount;
 		if (currentHealth <= 0) {
-			currentHealth = 0;
-			Debug.Log ("YOU'RE DEAD!");
+			
+			currentHealth = maxHealth;
+
+			// called on the Server, but invoked on the Clients
+			RpcRespawn();
 		}
 	}
 
 	void OnChangeHealth (int currentHealth) {
 		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick one at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
+		}
 	}
 }
